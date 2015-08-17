36d0987d136011b8c745c98269a4b84583478fe7
nithyamtcs <nithyanandhan.muthukumarasamy@tcs.com>
2015-08-17 20:31:20 +0530

    cachematrix.R
    Final version for peer review

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are couple of function in this file.
+## makeCacheMatrix - It creates a special matrix with function objects.
+## cacheSolve - It performs the matrix inverse using the special matrix passed as an argument
 
-## Write a short comment describing this function
+## It returns a special (4 X 1) matrix containing function as values.
+## These functions are used to store & retreive a invertible matrix & its inverse from cache.
+## The functions should be accessed something like m[["<func_name>",1]](...)
+## It reset the matrix and its inverse in cache only if the new matrix is not identical
 
 makeCacheMatrix <- function(x = matrix()) {
     mInverse <- NULL
@@ -19,7 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function uses the special matrix to perform the inverse of matrix.
+## If the inverse already available in cache, it returns it
+## Otherwise, performs the inverse using solve() method.
 
 cacheSolve <- function(x, ...) {
     inverseM <- x[["getInverse",1]]()
